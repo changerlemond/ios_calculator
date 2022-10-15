2be02da8b98ebd7cc95ba5c8f4f081562f0418f7
Claire Jung <83348294+jungclaire@users.noreply.github.com>
2022-10-15 15:19:21 +0900

    Initial Commit

---
+++ ios_calculator/ios_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_calculatorApp.swift
+//  ios_calculator
+//
+//  Created by jungclaire on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct ios_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
