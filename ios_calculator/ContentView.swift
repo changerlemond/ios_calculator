83f822cec3b32caf84527b13eab3cb3df93645ed
Claire Jung <83348294+jungclaire@users.noreply.github.com>
2022-10-17 02:39:02 +0900

    for문 사용해서 리팩토링

--- ios_calculator/ContentView.swift
+++ ios_calculator/ContentView.swift
@@ -7,7 +7,92 @@
 
 import SwiftUI
 
+enum ButtonType: String {
+    case first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, zero
+    case dot, equal, plus, minus, multiple, devide
+    case percent, opposite, clear
+    
+    var buttonDisplayName: String {
+        switch self {
+        case .first :
+            return "1"
+        case .second :
+            return "2"
+        case .third :
+            return "3"
+        case .fourth :
+            return "4"
+        case .fifth :
+            return "5"
+        case .sixth :
+            return "6"
+        case .seventh :
+            return "7"
+        case .eighth :
+            return "8"
+        case .nineth :
+            return "9"
+        case .zero :
+            return "0"
+        case .dot :
+            return "."
+        case .equal :
+            return "="
+        case .plus :
+            return "+"
+        case .multiple :
+            return "X"
+        case .devide :
+            return "÷"
+        case .opposite :
+            return "÷/-"
+        case .percent :
+            return "%"
+        case .clear :
+            return "C"
+        default:
+            return "?"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .first, .second, .third, .fourth, .fifth, .sixth, .seventh, .eighth, .nineth, .dot:
+            return Color("NumberButton")
+        case .equal, .plus, .minus, .multiple, .devide:
+            return Color.orange
+        case .percent, .opposite, .clear:
+            return Color.gray
+        default:
+            return Color.gray
+        }
+    }
+    
+    var forgroundColor: Color {
+        switch self {
+        case .first, .second, .third, .fourth, .fifth, .sixth, .seventh, .eighth, .nineth, .dot, .equal, .plus, .minus, .multiple, .devide:
+            return Color.white
+        case .percent, .opposite, .clear:
+            return Color.black
+        default:
+            return Color.black
+        }
+    }
+    
+}
+
 struct ContentView: View {
+    
+    @State private var totalNumber: String = "0"
+    
+    private let buttonData: [[ButtonType]] = [
+        [.clear, .opposite, .percent, .devide],
+        [.seventh, .eighth, .nineth, .multiple],
+        [.fourth, .fifth, .sixth, .minus],
+        [.first, .second, .third, .plus],
+        [.zero, .zero, .dot, .equal],
+    ]
+    
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
@@ -15,192 +100,36 @@ struct ContentView: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    Text("331,460")
+                    Text(totalNumber)
                         .padding()
                         .foregroundColor(.white)
                         .font(.system(size: 70))
                 }
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        Text("C")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.gray)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
+                
+                ForEach(buttonData, id: \.self) {
+                    line in
+                    HStack {
+                        ForEach(line, id: \.self) {
+                            item in
+                            Button {
+                                if (totalNumber != "0" && item.backgroundColor == Color("NumberButton")) {
+                                    totalNumber += item.buttonDisplayName
+                                } else if (totalNumber == "0" && item.backgroundColor == Color("NumberButton")) {
+                                    totalNumber = item.buttonDisplayName
+                                } else if (item.buttonDisplayName == "C") {
+                                    totalNumber = "0"
+                                }
+                            } label: {
+                                Text(item.buttonDisplayName)
+                                    .frame(width: 80,
+                                           height: 80)
+                                    .background(item.backgroundColor)
+                                    .cornerRadius(40)
+                                    .foregroundColor(item.forgroundColor)
+                                    .font(.system(size: 33))
+                            }
+                        }
                     }
-                    Button {
-                        
-                    } label: {
-                        Text("+/-")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.gray)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("%")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.gray)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("÷")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.orange)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                }
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        Text("7")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.gray)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("8")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.gray)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("9")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.gray)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("X")
-                            .frame(width: 80,
-                            height: 80)
-                            .background(.orange)
-                            .cornerRadius(40)
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                }
-                HStack {
-                    Text("4")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("5")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("6")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("-")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.orange)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                }
-                HStack {
-                    Text("1")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("2")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("3")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("+")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.orange)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                }
-                HStack {
-                    Text("0")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("0")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text(".")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.gray)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
-                    Text("=")
-                        .frame(width: 80,
-                        height: 80)
-                        .background(.orange)
-                        .cornerRadius(40)
-                        .foregroundColor(.white)
-                        .font(.system(size: 33))
                 }
             }
         }
